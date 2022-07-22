4d53074a0ff1cd86996bb31c0d78b3d9f48247f9
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2022-07-22 17:41:07 +0530

    template importing

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse(f'single project : {pk}')
+    return render(request, 'single-project.html')
