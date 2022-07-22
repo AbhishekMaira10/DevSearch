edfd604056b5223740a756aa7968e36ddc55ab31
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2022-07-22 15:32:23 +0530

    urls and views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    return HttpResponse('Here are our projects')
+
+def project(request, pk):
+    return HttpResponse(f'single project : {pk}')
